8534a369af7baa897b991cd9046f85fe56f0b0e5
Colin Zhao <colin@townsquaredigital.com>
2026-03-14 18:13:14 -0400

    add setup screen

--- src/components/Setup.jsx
+++ src/components/Setup.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import icon from '../assets/icon.png'
 
 export function Setup({ error, isSettingUp, onSetup }) {
     return (
         <main className="setup-screen">
             <section className="setup-card">
+                <img alt="App logo" className="setup-logo" src={icon} />
                 <h1>Setup</h1>
                 <p>
                     This will create a `local-data` folder with full permission and
